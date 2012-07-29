ebe454fc5209c81e8407bd445201fc8560667b39
Stanford Humanities <stanford.humanities.events@gmail.com>
2012-07-29 14:05:01 -0700

    Boilerplate

---
+++ fixer_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2011, The Board of Regents of Leland Stanford, Jr. University
+# All rights reserved. See LICENSE.
+# Author: Christine Williams <christine.bennett.williams@gmail.com>
+
+import unittest
+
+
+class FixerTest(unittest.TestCase):
+  pass
+  
+  
+if __name__ == '__main__':
+  unittest.main()
