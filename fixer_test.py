62817c577286ea0391b24188d5335b9be13020bd
Stanford Humanities <stanford.humanities.events@gmail.com>
2012-07-29 15:07:44 -0700

    Add comments to FixTemplates and added a NeedsConversion function

--- fixer_test.py
+++ fixer_test.py
@@ -9,12 +9,33 @@ import unittest
 import re
 
 def FixTemplates(source, fragments={}, title=''):
+  """Fixes up RapidWeaver published html files into valid Cheetah templates.
+  
+  The algorithm is:
+  1. Replace $( with \$(, because this innocent-looking jQuery syntax confuses Cheetah.
+  2. Replace <!--code:fragment-name--> with fragments[fragment-name].
+  3. Replace everything between <title> and | with title.
+  
+  Args:
+    source: The original HTML published by RapidWeaver.
+    fragments: A dictionary of fragment-name to fragment content. Any comments in source
+      that match <!--code:fragment-name--> are replaced by looking up fragment-name in
+      this dictionary and replacing the whole comment with the value.
+    title: Template fragment to insert between <title> and |.
+    
+  Returns:
+    A valid Cheetah template.
+  """
   fixed = source.replace('$(', r'\$(')
   fixed = re.sub(r'<title>[^|]+\|', '<title>' + title + '|', fixed)
   def GetFragment(matches):
     return fragments[matches.group(1)]
   fixed = re.sub(r'<!--code:(\w+)-->', GetFragment, fixed)
   return fixed
+  
+def NeedsConversion(source):
+  """Check source to see if it was just publish by RapidWeaver and not yet fixed."""
+  return '<!--code' in source or re.search(r'[^\\]\$\(', source)
 
 
 class FixerTest(unittest.TestCase):
@@ -35,6 +56,12 @@ class FixerTest(unittest.TestCase):
     self.assertEquals('stuff <title>{{code}}| More stuff</title>',
                       FixTemplates('stuff <title>Events | More stuff</title>',
                                    title='{{code}}'))
+                                   
+  def testUnfixed(self):
+    self.assertTrue(NeedsConversion('stuff $(badness)'))
+    
+  def testFixed(self):
+    self.assertFalse(NeedsConversion(r'stuff \$(badness)'))
   
   
 if __name__ == '__main__':
